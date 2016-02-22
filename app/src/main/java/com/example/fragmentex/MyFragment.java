244e8edf9fa50dd61c1d9da865d9b46b2e449ca6
HarshitSomal <hkaurece@gmail.com>
2016-02-21 16:08:08 -0800

    Initial commit

---
+++ app/src/main/java/com/example/fragmentex/MyFragment.java
@@ -0,0 +1,21 @@
+package com.example.fragmentex;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MyFragment extends Fragment{
+
+	/* (non-Javadoc)
+	 * @see android.app.Fragment#onCreateView(android.view.LayoutInflater, android.view.ViewGroup, android.os.Bundle)
+	 */
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		return inflater.inflate(R.layout.my_fragment,container,false);
+	}
+
+}
